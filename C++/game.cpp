a82eaa92c9a5f25e2deb1093095cb90c22bb27f1
abdalah124 <asd028990@gmail.com>
2024-11-08 13:49:44 +0200

    modified game.cpp

---
+++ C++/game.cpp
@@ -0,0 +1,258 @@
+#include<iostream>
+using namespace std;
+
+int read()
+{
+  system("color 1f");
+  cout<<"\t"<<"----------------------------------------------------------------------------------"<<endl;
+  cout<<"\t"<<"-------------- welcome to who  will win the one pound ----------------------------"<<endl;
+  cout<<"\t"<<"----------------------------------------------------------------------------------"<<endl;
+  cout<<"\t"<<"-"<<"this game about some  questions if you answered you will win one pound "<<"-"<<endl;
+  cout<<"\t"<<"-"<<"if you want to start agame press number 1 key"<<"-"<<endl;
+  cout<<"\t"<<"----------------------------------------------------------------------------------"<<endl;
+ int num;
+  cin>> num;
+  return num ;
+}
+bool print()
+{
+int num ;
+ num = read();
+
+  if (num == 1)
+ {
+  cout<<"i son of water but if you left me there i will die who am i"<<endl;
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl;        
+         cout<<"(1)" <<"----- water ------"<<"    "<<"      (2)" <<"----- stons ------"<<endl;  
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl;               
+         cout<<"(3)" <<"----- ice ------" <<"    "<<"       (4)" <<"----- sand ------"<<endl;       
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl; 
+        
+        cin>>num;
+        if(num == 3)
+        {
+          system("color 2f");
+          cout<<"right answer your balance is 10 krosh\n";
+          cout<<endl;
+          cout<<"what is the black gold"<<endl;
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl;        
+         cout<<"(1)" <<"----- platin ------"<<"    "<<"      (2)" <<"----- gold ------"<<endl;  
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl;               
+         cout<<"(3)" <<"----- coal ------" <<"    "<<"       (4)" <<"----- oil ------"<<endl;       
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl; 
+         cin>>num;
+        if(num == 4)
+        {
+           system("color 2f");
+          cout<<"right answer your balance is 20 krosh\n";
+          cout<<endl;
+          cout<<"you can see me but you can't catch me who am i "<<endl;
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl;        
+         cout<<"(1)" <<"----- water ------"<<"    "<<"      (2)" <<"----- air ------"<<endl;  
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl;               
+         cout<<"(3)" <<"----- shadow ------" <<"    "<<"       (4)" <<"----- light ------"<<endl;       
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl; 
+         cin>>num;
+         if (num==3)
+         {
+           system("color 2f");
+          cout<<"right answer your balance is 30 krosh\n";
+          cout<<endl;
+          cout<<" i can't walk but if you hit me i will walk who am i "<<endl;
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl;        
+         cout<<"(1)" <<"----- nail ------"<<"    "<<"      (2)" <<"----- air ------"<<endl;  
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl;               
+         cout<<"(3)" <<"----- pin ------" <<"    "<<"       (4)" <<"----- donky ------"<<endl;       
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl; 
+         cin>>num;
+         if (num == 1)
+         {
+            system("color 2f");
+          cout<<"right answer your balance is 40 krosh\n";
+          cout<<endl;
+          cout<<" i eat but i don't sate who am i "<<endl;
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl;        
+         cout<<"(1)" <<"----- hole ------"<<"    "<<"      (2)" <<"----- air ------"<<endl;  
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl;               
+         cout<<"(3)" <<"----- fire ------" <<"    "<<"       (4)" <<"----- donky ------"<<endl;       
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl; 
+         cin>>num;
+         if(num == 3)
+         {
+            system("color 2f");
+          cout<<"right answer your balance is 50 krosh\n";
+          cout<<endl;
+          cout<<" i dont have afoot but i break through the wall to reatch to ears who am i  "<<endl;
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl;        
+         cout<<"(1)" <<"----- light  ------"<<"    "<<"      (2)" <<"----- sound ------"<<endl;  
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl;               
+         cout<<"(3)" <<"----- fire ------" <<"    "<<"       (4)" <<"----- pepper ------"<<endl;       
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl; 
+         cin>>num;
+         if(num == 2)
+         {
+             system("color 2f");
+          cout<<"right answer your balance is 60 krosh\n";
+          cout<<endl;
+          cout<<" i can  break through the glass but i can't broken it who am i  "<<endl;
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl;        
+         cout<<"(1)" <<"----- light  ------"<<"    "<<"      (2)" <<"----- sound ------"<<endl;  
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl;               
+         cout<<"(3)" <<"----- water ------" <<"    "<<"       (4)" <<"----- gas ------"<<endl;       
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl; 
+         cin>>num;
+         if(num == 1)
+         {
+               system("color 2f");
+          cout<<"right answer your balance is 70 krosh\n";
+          cout<<endl;
+          cout<<" i have alot of teeth but i don't bite who am i "<<endl;
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl;        
+         cout<<"(1)" <<"----- mouth  ------"<<"    "<<"      (2)" <<"----- teeth ------"<<endl;  
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl;               
+         cout<<"(3)" <<"----- dog ------" <<"    "<<"       (4)" <<"----- comb ------"<<endl;       
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl; 
+         cin>>num;
+         if(num == 4)
+         {
+                 system("color 2f");
+          cout<<"right answer your balance is 80 krosh\n";
+          cout<<endl;
+          cout<<"  you hit me without reasone who am i "<<endl;
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl;        
+         cout<<"(1)" <<"-----  nail ------"<<"    "<<"      (2)" <<"----- ball ------"<<endl;  
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl;               
+         cout<<"(3)" <<"----- donky ------" <<"    "<<"       (4)" <<"----- pepper ------"<<endl;       
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl; 
+         cin>>num;
+         if(num == 2)
+         {
+                system("color 2f");
+          cout<<"right answer your balance is 90 krosh\n";
+          cout<<endl;
+          cout<<" i can carry a ton put i can't carry a nail   "<<endl;
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl;        
+         cout<<"(1)" <<"-----  man ------"<<"    "<<"      (2)" <<"----- iron ------"<<endl;  
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl;               
+         cout<<"(3)" <<"----- sea ------" <<"    "<<"       (4)" <<"----- woden ------"<<endl;       
+         cout<<"__________________________"<<"    "<<"__________________________"<<endl; 
+         cin>>num;
+         if(num == 3)
+         {
+                system("color 2f");
+          cout<<"right answer your balance is 100 krosh\n";
+          cout<<"congratulations you win one pound\n";
+
+         }
+         else
+         {
+          system("color 4f");
+          cout<<"wrong answer your balance is 0 krosh\n";
+          print();
+         }
+         }
+         else
+         {
+          system("color 4f");
+          cout<<"wrong answer your balance is 0 krosh\n";
+          print();
+         }
+         }
+         else
+         {
+          system("color 4f");
+          cout<<"wrong answer your balance is 0 krosh\n";
+          print();
+         }
+         }
+         else
+         {
+          system("color 4f");
+          cout<<"wrong answer your balance is 0 krosh\n";
+          print();
+         }
+         }
+         else
+         {
+            system("color 4f");
+          cout<<"wrong answer your balance is 0 krosh\n";
+          print();
+         }
+
+         }
+         else
+         {
+            system("color 4f");
+          cout<<"wrong answer your balance is 0 krosh\n";
+          print();
+         }
+         }
+         else
+         {
+          system("color 4f");
+          cout<<"wrong answer your balance is 0 krosh\n";
+          print();
+         }
+         }
+         else
+         {
+           system("color 4f");
+          cout<<"wrong answer your balance is 0 krosh\n";
+          print();
+         }
+         }
+         
+         else
+         {
+          system("color 4f");
+          cout<<"wrong answer your balance is 0 krosh\n";
+          print();
+         }
+        }
+        else
+        {
+          system("color 4f");
+          cout<<"wrong answer your balance is 0 krosh\n";
+          print();
+        }
+        }
+        else
+        {
+          system("color 4f");
+          cout<<"wrong answer your balance is 0 krosh\n";
+          print();
+        }
+ 
+   
+ 
+  return 0;
+}
+char startgame()
+{
+	char again='Y';
+	do
+	{
+		
+		cout << endl << "Do you want to play again Y/N" << endl;
+
+		cin >> again;
+		cout << endl;
+		system("cls");
+		system("color 0f");
+		print();
+
+	} while (again == 'y' || again == 'Y');
+
+
+	return again;
+}
+
+int main()
+{
+ 
+  print();
+  startgame();
+    
+  return 0;
+
+}
